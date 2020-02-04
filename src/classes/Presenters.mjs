1b6ee7897af7c47dc00c2ae20863cda60b1a716c
Cameron Welter <camjwelt@gmail.com>
2020-02-04 19:36:55 +0000

    fix presenters

--- src/classes/Presenters.mjs
+++ src/classes/Presenters.mjs
@@ -1,18 +1,17 @@
 export default class DataPresenters {
   static presentSongMetadata (songInfo) {
     return {
-      id: songInfo.path || 'N/A',
+      id: songInfo.path || null,
       type: 'song',
       attributes: {
-        album: songInfo.album || 'N/A',
-        albumArtist: songInfo.albumArtist || 'N/A',
-        artist: songInfo.artist || 'N/A',
-        date: songInfo.date || 'N/A',
-        file: songInfo.path || 'N/A',
-        genre: songInfo.genre || 'N/A',
-        position: songInfo.position || 'N/A',
-        duration: songInfo.duration || 'N/A',
-        title: songInfo.title || 'N/A',
+        album: songInfo.album || null,
+        albumArtist: songInfo.albumArtist || null,
+        artist: songInfo.artist || null,
+        date: songInfo.date || null,
+        genre: songInfo.genre || null,
+        position: songInfo.position ?? null,
+        duration: songInfo.duration ?? 0,
+        title: songInfo.title || null,
       },
     }
   }
